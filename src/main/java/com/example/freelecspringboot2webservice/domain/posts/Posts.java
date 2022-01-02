f9f82fc62bcff10752fa3d503e7b73dd524ec333
ChoiJaeEun <poungki0823@gmail.com>
2022-01-02 23:03:12 +0900

    Posts domain

---
+++ src/main/java/com/example/freelecspringboot2webservice/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.example.freelecspringboot2webservice.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+// 주요 annotation 을 class 에 가깝게!!
+@Getter
+@NoArgsConstructor  // lombok
+@Entity  // JPA annotation : 실제 DB table 과 매칠 될 class
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
