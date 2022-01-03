067b42d5dd186a8040f779a9c957cccbd5813808
ChoiJaeEun <poungki0823@gmail.com>
2022-01-03 09:19:02 +0900

    get posts, update service method

--- src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.example.freelecspringboot2webservice.service.posts;
 
+import com.example.freelecspringboot2webservice.domain.posts.Posts;
 import com.example.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.example.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
+import com.example.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +19,19 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
 }
+
