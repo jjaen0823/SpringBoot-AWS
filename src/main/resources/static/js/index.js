f2c838be933ae62821699cf803b91ef22438c3b1
ChoiJaeEun <poungki0823@gmail.com>
2022-01-05 11:22:38 +0900

    button event 처리

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,56 @@
+/* var main = {} 사용 이유
+* 다른 파일에 동일한 function name 이 있을 경우, 나중에 로딩되는 함수를 먼저 로딩 된 함수가 덮어 쓴다.
+* => 해당 파일의 유효범위를 만들어 사용한다!!
+* */
+const main = {
+    init : function () {
+        let _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+    },
+    save : function () {
+        let data = {
+            title: $('#title').val(),
+            content: $('#content').val(),
+            author: $('#author').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert("Complete save your posts!");
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        let data = {
+            title: $('#title').val(),
+            content: $('#content').val(),
+        };
+        const id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert("Complete update your posts!");
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
