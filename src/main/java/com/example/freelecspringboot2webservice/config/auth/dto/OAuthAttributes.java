bf7149e04ad790043c34283bce2cd73b1d85a625
ChoiJaeEun <poungki0823@gmail.com>
2022-01-08 23:58:28 +0900

    naver login

--- src/main/java/com/example/freelecspringboot2webservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/dto/OAuthAttributes.java
@@ -28,7 +28,19 @@ public class OAuthAttributes {
         if ("google".equals(registrationId)) {
             return ofGoogle(userNameAttributeName, attributes);
         }
-        return null;
+        return ofNaver("id", attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture(String.valueOf(response.get("profile_image")))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -40,6 +52,7 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
 
     public User toEntity() {
         return User.builder()
