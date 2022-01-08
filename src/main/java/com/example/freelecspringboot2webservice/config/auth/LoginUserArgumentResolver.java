98f9f08ad7362bdbd150468d1a2510562d6f807b
ChoiJaeEun <poungki0823@gmail.com>
2022-01-08 22:44:16 +0900

    implements HandlerMethodArgumentResolver: 조건에 맞는 method 가 있다면 구현체가 지정한 값으로 해당 method 의 parameter 로 넘길 수 있음

---
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.example.freelecspringboot2webservice.config.auth;
+
+import com.example.freelecspringboot2webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getMethodAnnotation(LoginUser.class) != null;  // LoginUser annotation 여부
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());  // parameter type 이 SessionUser 이어야 함!
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
