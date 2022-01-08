37d9a615c9850acdf34579a5ea32af60bc37561a
ChoiJaeEun <poungki0823@gmail.com>
2022-01-08 22:35:29 +0900

    social login 했을 때 후처리를 해주는 OAuth2UserService와 user 정보를 담을 OAuthAttributes class

---
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,66 @@
+package com.example.freelecspringboot2webservice.config.auth;
+
+import com.example.freelecspringboot2webservice.config.auth.dto.OAuthAttributes;
+import com.example.freelecspringboot2webservice.config.auth.dto.SessionUser;
+import com.example.freelecspringboot2webservice.domain.user.User;
+import com.example.freelecspringboot2webservice.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    /* loadUser
+    * google, naver(social)로 부터 받은 userRequest data 에 대한 후처리 method
+    * 해당 method 종료 시, @AuthenticationPrincipal annotation 이 만들어짐
+    * */
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        System.out.println("registrationId >>> " + registrationId);
+        System.out.println("userNameAttributeName >>> " + userNameAttributeName);
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        System.out.println("attributes >>> " + attributes);
+
+        User user = saveOrUpdate(attributes);  // repository 에 있다면 update
+        httpSession.setAttribute("user", new SessionUser(user));  // Session 생성
+        /* print OAuth2 Info */
+
+        System.out.println("oAuth2User.getAttributes() >>> " + oAuth2User.getAttributes());
+        System.out.println("user.getName() >>> " + user.getName());
+        System.out.println("Session user >>> " + httpSession.getAttribute("user"));
+        System.out.println(" >>> ");
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))  // repository 에 있다면 update
+                .orElse(attributes.toEntity());  // 없으면 toEntity(): User Entity 생성
+
+        return userRepository.save(user);
+    }
+}
