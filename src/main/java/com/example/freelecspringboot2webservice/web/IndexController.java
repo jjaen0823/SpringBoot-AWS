2c90de02b7f7dceb1ca870b0dad5a0a424ba7e15
ChoiJaeEun <poungki0823@gmail.com>
2022-01-08 22:46:36 +0900

    @LoginUser 를 통해 Session User 가져오기

--- src/main/java/com/example/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/example/freelecspringboot2webservice/web/IndexController.java
@@ -1,25 +1,39 @@
 package com.example.freelecspringboot2webservice.web;
 
+import com.example.freelecspringboot2webservice.config.auth.LoginUser;
+import com.example.freelecspringboot2webservice.config.auth.dto.SessionUser;
 import com.example.freelecspringboot2webservice.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {  //, @LoginUser SessionUser user
         /* mustache
         * 경로: src/main/resources/templates
         * 확장자: .mustache
         * */
         model.addAttribute("posts", postsService.findAllDesc());
-        return "index";  // src/main/resources/templates.mustache
+
+        // login 성공 시, OAuth2User loadUser 에서 SessionUser 저장 -> 로그인 성공 시 httpSession 에서 user 정보를 가져옴
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {  // Session 에 user 가 있을 때만 View 에 넘겨줌
+            System.out.println(user);
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";  // src/main/resources/index.mustache
     }
 
     @GetMapping("/posts/save")
