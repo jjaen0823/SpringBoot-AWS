ed6d078bb7c9cf3c29651dd1f6de1c4b99d6443a
ChoiJaeEun <poungki0823@gmail.com>
2022-01-10 13:57:03 +0900

    LoggerInterceptor 생성, 등록

--- src/main/java/com/example/freelecspringboot2webservice/config/WebConfig.java
+++ src/main/java/com/example/freelecspringboot2webservice/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.example.freelecspringboot2webservice.config;
 
 import com.example.freelecspringboot2webservice.config.auth.LoginUserArgumentResolver;
+import com.example.freelecspringboot2webservice.domain.posts.interceptor.LoggerInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 //        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    // Interceptor 추가
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new LoggerInterceptor());
     }
 }
