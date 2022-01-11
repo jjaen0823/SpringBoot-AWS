78c76ab6159cde42566083a4effd420fea99e09d
ChoiJaeEun <poungki0823@gmail.com>
2022-01-12 00:58:59 +0900

    delete useless code

--- src/main/java/com/example/freelecspringboot2webservice/Application.java
+++ src/main/java/com/example/freelecspringboot2webservice/Application.java
@@ -2,7 +2,6 @@ package com.example.freelecspringboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing  // @SpringBootApplication 와 분리 !!
 @SpringBootApplication
--- src/main/java/com/example/freelecspringboot2webservice/config/WebConfig.java
+++ src/main/java/com/example/freelecspringboot2webservice/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.freelecspringboot2webservice.config;
 
 import com.example.freelecspringboot2webservice.config.auth.LoginUserArgumentResolver;
-import com.example.freelecspringboot2webservice.domain.posts.interceptor.LoggerInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -21,12 +20,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 //        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(loginUserArgumentResolver);
-    }
-
-    // Interceptor 추가
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-//        WebMvcConfigurer.super.addInterceptors(registry);
-        registry.addInterceptor(new LoggerInterceptor());
     }
 }
